da7ea67da51016b6955dc0855bd42a0322851d3e
Aasdfr <96871438+Aasdfr@users.noreply.github.com>
2021-12-30 13:05:21 +0100

    Create a.js

---
+++ a.js
@@ -0,0 +1,14 @@
+let elemento = document.createElement("div");
+
+document.createElement('div');
+elemento.className='Quasar';
+
+elemento.style.position='absolute';
+elemento.style.top=0;
+elemento.style.left='50%';
+
+elemento.style.width='200px';
+elemento.style.height='200px';
+elemento.style.backgroundImage = 'url("https://u.cubeupload.com/MutalistVirus/RealGold128.png")';
+elemento.style.backgroundRepeat = 'no-repeat';
+document.body.appendChild(elemento);
